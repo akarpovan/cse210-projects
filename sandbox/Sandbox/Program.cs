f0a60b2a762f9a63980360e1c67e2cb06bc3158c
Anna Karpova <akarpovan@gmail.com>
2025-05-06 22:59:28 -0500

    Prueba

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox example Project.");
+        Console.WriteLine("Hello World! This is the Sandbox ¡¡¡¡¡¡ Project.");
     }
 }
