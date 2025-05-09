c9bfe3f625fb16a010785b4c5ef85433a81e1a00
Anna Karpova <akarpovan@gmail.com>
2025-05-08 22:46:11 -0500

    Added a program

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        //Ask the user for their first name.
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        //Ask the user for their last name.
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
