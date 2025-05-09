40753ad0ffb519f7f7d32a09733725f231519675
Anna Karpova <akarpovan@gmail.com>
2025-05-08 23:48:58 -0500

    Added the program

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string user_persent = Console.ReadLine();
+        int persent = int.Parse(user_persent);
+        string letter = "";
+
+        if (persent >= 90)
+        {
+            letter = "A";
+        }
+        else if (persent >= 80)
+        {
+            letter = "B";
+        }
+        else if (persent >= 70)
+        {
+            letter = "C";
+        }
+        else if (persent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //Stretch Challenge 1
+        string sign = "";
+        int last_digit = persent % 10;
+
+        if (last_digit >= 7)
+        {
+            sign = "+";
+        }
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";   
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        if (persent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        } 
+        else
+        {
+            Console.WriteLine("Don't get discouraged! You can do it!");
+        }
     }
 }
