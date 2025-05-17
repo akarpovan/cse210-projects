6e47596fc56c972a5bd3fc19d392ca5a2b8b8933
Anna Karpova <akarpovan@gmail.com>
2025-05-16 20:47:18 -0500

    Added one new class

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        //Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        //Create the Constructors
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        //Create the Getters and Setters
+        Fraction f5 = new Fraction();
+        f5.SetTop(6);
+        f5.SetBottom(7);
+        Console.WriteLine(f5.GetTop());
+        Console.WriteLine(f5.GetBottom());
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
+        f5.SetTop(2);
+        f5.SetBottom(8);
+        Console.WriteLine(f5.GetTop());
+        Console.WriteLine(f5.GetBottom());
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
+
     }
 }
